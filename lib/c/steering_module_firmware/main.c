baf883338cb29295a3a8ebd9611112101d327cfb
Elijah Roberts <eroberts00@gmail.com>
2023-01-15 09:33:48 -0800

    Add sterring module firmware template.

---
+++ lib/c/steering_module_firmware/main.c
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2023, Elijah Roberts
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+
+#ifndef PICO_DEFAULT_LED_PIN
+#error "Onboard LED is a required feature."
+#endif
+
+int main() {
+
+    // Setup the onboard LED pin.
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    // Initialize stdio.
+    stdio_init_all();
+
+    // Loop.
+    uint count = 0;
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        sleep_ms(500);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(500);
+        printf("Cycle: %d\n", count);
+        count++;
+    }
+    return 0;
+}
